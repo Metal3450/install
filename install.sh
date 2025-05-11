991ea4e9517cf9af50d5dd0fd2690b6aa1e514a7
Metal3450 <mark.kachanovski@gmail.com>
2025-05-11 07:21:13 +0000

    commit 1

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+sudo cp -r hyprland-void.conf /etc/xbps.d/
+sudo xbps-install -Su
+sudo xbps-install -S hyprland hyprlock wofi curl wget meson ninja make
+sudo xbps-install zip unzip nerd-fonts-symbols-ttf pulseaudio pavucontrol
+cp -r ./confs/* ~/.config 
+cp -r ./.bashrc ~/
+cd ..
+git clone https://github.com/Alexays/Waybar.git
+git clone https://github.com/LGFae/swww.git
+wget https://download.gnome.org/sources/gtkmm/3.24/gtkmm-3.24.0.tar.xz
