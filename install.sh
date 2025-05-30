c0b4ce0b9d47a5107c232d4665b6bb3bd6361085
Metal3450 <mark.kachanovski@gmail.com>
2025-05-30 17:56:03 +0000

    commit 9

--- install.sh
+++ install.sh
@@ -2,9 +2,9 @@
 
 sudo cp -r hyprland-void.conf /etc/xbps.d/
 sudo xbps-install -Su
-sudo xbps-install -S hyprland hyprlock wofi curl wget meson ninja make
-sudo xbps-install zip unzip nerd-fonts-symbols-ttf pulseaudio pavucontrol
-sudo xbps-install -Sy glibc-32bit libX11-32bit libXext-32bit libstdc++-32bit
+sudo xbps-install -Sy hyprland hyprlock wofi curl wget meson ninja make
+sudo xbps-install zip unzip nerd-fonts-symbols-ttf pulseaudio pavucontrol xorg-server-xwayland xorg-server-xwayland-32bit
+sudo xbps-install -Sy void-repo-multilib void-repo-nonfree glibc-32bit libX11-32bit libXext-32bit libstdc++-32bit libdrm-32bit libGL-32bit
 cp -r ./confs/* ~/.config 
 cp -r ./.bashrc ~/
 cd ..
