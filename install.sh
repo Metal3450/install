a101ffb1e0d46779e7e623f75b97cae0f79a8c8e
Metal3450 <mark.kachanovski@gmail.com>
2025-11-11 10:54:23 +0100

    x

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 sudo cp -r hyprland-void.conf /etc/xbps.d/
 sudo xbps-install -Su
-sudo xbps-install -y hyprland hyprlock wofi curl wget meson ninja make seatd elogind
+sudo xbps-install -y hyprland hyprlock wofi curl wget meson ninja make seatd elogind sddm
 sudo xbps-install zip unzip nerd-fonts-symbols-ttf alsa-utils alsa-lib alsa-plugins apulse xorg-server-xwayland xorg-server-xwayland-32bit mesa-32bit mesa-vulkan-intel-32bit libglvnd-32bit
 sudo xbps-install -y mesa-dri mesa-vulkan-intel libglvnd
 sudo xbps-install -y void-repo-multilib void-repo-nonfree glibc-32bit libX11-32bit libXext-32bit libstdc++-32bit libdrm-32bit libGL-32bit
@@ -12,5 +12,5 @@ git clone https://github.com/Alexays/Waybar.git
 git clone https://github.com/LGFae/swww.git
 git clone https://github.com/vinceliuice/Graphite-gtk-theme
 sudo ln -s /etc/sv/seatd /var/service
+sudo ln -s /etc/sv/sddm /var/service
 sudo ln -s /etc/sv/elogind /var/service
-
